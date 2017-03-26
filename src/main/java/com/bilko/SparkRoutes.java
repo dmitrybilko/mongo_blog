2c237ffc7d86fea0865f51342255464bbb63c055
Dmitry Bilko <boopochta@gmail.com>
2017-03-26 22:00:46 +0300

    Handle different Spark routes

---
+++ src/main/java/com/bilko/SparkRoutes.java
@@ -0,0 +1,13 @@
+package com.bilko;
+
+import spark.Spark;
+
+public class SparkRoutes {
+
+    public static void main(final String[] args) {
+
+        Spark.get("/", ((request, response) -> "Hello World"));
+        Spark.get("/test", ((request, response) -> "Hello Test"));
+        Spark.get("/echo/:thing", ((request, response) -> request.params(":thing")));
+    }
+}
